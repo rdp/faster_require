437a52ff462bf19ebc8e57a87a2f5a7799db242e
rdp <rogerpack2005@gmail.com>
2010-01-15 16:50:48 -0700

    first signs of life

--- lib/fast_require.rb
+++ lib/fast_require.rb
@@ -1,48 +1,56 @@
-require 'sane' # TODO take out
-require 'ruby-debug'
-
-
 module FastRequire
-  
+
   @@loc = File.expand_path('~/.ruby_fast_require_location')
   if File.exist? (@@loc)
     @@require_locs = Marshal.restore( File.open(@@loc, 'rb') {|f| f.read})
   else
     @@require_locs = {}
   end
-  
+
   at_exit {
     FastRequire.save
   }
-  
+
   def self.save
-    File.open(@@loc, 'wb'){|f| f.write Marshal.dump(@@require_locs)}  
+    File.open(@@loc, 'wb'){|f| f.write Marshal.dump(@@require_locs)}
   end
-  
+
   def self.clear!
     require 'fileutils'
     FileUtils.rm @@loc if File.exist? @@loc
-    @@require_locs.clear 
+    @@require_locs.clear
   end
-  
-  def require lib
-    puts lib
+
+  def require_cached lib
     if a = @@require_locs[lib]
-      puts 'miss'
-      eval File.open(a, 'rb') {|f| f.read} # binread
+      begin
+        puts 'doing eval', a
+        eval File.open(a, 'rb') {|f| f.read} # binread
+        $LOADED_FEATURES << a
+      rescue => e
+        debugger
+        3
+      end
     else
-      puts 'hit'
-      
       old = $LOADED_FEATURES.dup
-      original_require lib
-      new = $LOADED_FEATURES - old
-      @@required[lib] = new.last
+      puts lib.to_s      
+      if(originally_require lib)
+        new = $LOADED_FEATURES - old
+        @@require_locs[lib] = new.last      
+      end# how could this fail, though?
     end
+    
   end
 end
 
 module Kernel
-  alias :original_require :require
-  extend FastRequire   # overwrite require...
+  if(defined?(@already_using_fast_require))
+    raise 'cant require it twice'
+  else
+    @already_using_fast_require = true
+  end
+  alias :originally_require :require
+  include FastRequire   # overwrite require...
+  alias :require :require_cached
 end
 
--- spec/spec.fast_require.rb
+++ spec/spec.fast_require.rb
@@ -1,46 +1,86 @@
-raise if defined?(FastRequire)
+require 'faster_rubygems' if RUBY_VERSION < '1.9'
 require 'sane'
+assert !defined?(FastRequire) # so that we can loadup our unit tests sanely, using the old way LOL.
+require 'sane'
+require 'ruby-debug'
 require 'spec/autorun'
+require 'benchmark'
 
-$: << File.dirname(__FILE__)  
-load '../lib/fast_require.rb'
+$: << File.dirname(__FILE__)
 
 describe "faster requires" do
-  
-  before do # each
-    FastRequire.clear!    
-    load '../lib/fast_require.rb'      
+
+  def with_file(filename = 'test')
+      FileUtils.touch filename + '.rb'
+      yield
+      FileUtils.rm filename + '.rb'
+  end
+
+  it "should be able to go one deep" do
+    with_file do
+      require 'test' # should not blow
+    end
+  end
+
+
+  it "should be faster" do
+    slow = Benchmark.realtime { system("#{OS.ruby_bin} slow.rb")}
+    Benchmark.realtime { system("#{OS.ruby_bin} fast.rb")} # warmup
+    fast = Benchmark.realtime { system("#{OS.ruby_bin} fast.rb")}
+    pps 'fast', fast, 'slow', slow
+    assert fast*2 < slow
   end
   
-  def with_file(filename)
-    FileUtils.touch filename + '.rb'
-    yield
-    FileUtils.rm filename + '.rb'
+  it "should not save if it hasn't changed"
+  it "should cache when requires have already been done instead of calling require on them again"
+  it "should know which requires are currently active and avoid calling require on them again"
+
+  it "should do two of the same requires" do
+    with_file('test') do
+      require 'test'
+      require 'test'      
+    end
   end
-  
-  it "should require files still" do
-    with_file('file1') { require 'file1'}    
-  end
-  
-  it "should require .so files still" do
-    # ruby-prof gem
-    require 'ruby_prof' # .so
-  end
-  
-  it "should add them to $LOADED_FEATURES" do
-    with_file('file2') {require 'file2'}
-    assert ($LOADED_FEATURES.grep(/file2.rb/)).length > 0
-  end
-  
-  it "should work with and without rubygems, esp. in 1.8"
-  
-  it "should have a faster require method--faster, my friend, faster!"
-  
-  it "should save a file" do
-    FastRequire.clear!
-    loc = File.expand_path('~/.ruby_fast_require_location')
-    assert !File.exist?(loc)
-    FastRequire.save
-    assert File.exist?(loc)
-  end
+
+
+    require_relative '../lib/fast_require'
+    before do # each
+      ::FastRequire.clear!
+    end
+
+    it "should be able to go two deep" do
+      
+    end
+
+    
+    it "should require files still" do
+      with_file('file1') { require 'file1'}
+    end
+
+    it "should require .so files still" do
+      # ruby-prof gem
+      require 'ruby_prof' # .so
+    end
+
+    it "should add them to $LOADED_FEATURES" do
+      with_file('file2') {require 'file2'}
+      assert ($LOADED_FEATURES.grep(/file2.rb/)).length > 0
+    end
+
+    it "should work with and without rubygems, esp. in 1.8" do
+      # run these tests in 1.8...hmm...
+      # maybe with and
+    end
+
+    it "should have a faster require method--faster, my friend, faster!"
+
+    it "should save a file" do
+      FastRequire.clear!
+      loc = File.expand_path('~/.ruby_fast_require_location')
+      assert !File.exist?(loc)
+      FastRequire.save
+      assert File.exist?(loc)
+    end
+
+    it "should override the gem method"
 end
