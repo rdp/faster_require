0504ecef9ee72405d0ac21d5c0c6268c967343de
rdp <rogerpack2005@gmail.com>
2010-01-15 15:13:29 -0700

    tests wurk

--- lib/fast_require.rb
+++ lib/fast_require.rb
@@ -1,6 +1,42 @@
-module Kernel
-  alias :original_require :require
+require 'sane' # TODO take out
+require 'ruby-debug'
+
+
+module FastRequire
+  
+  @@loc = File.expand_path('~/.ruby_fast_require_location')
+  if File.exist? (@@loc)
+    @@require_locs = Marshal.restore( File.open(@@loc, 'rb') {|f| f.read})
+  else
+    @@require_locs = {}
+  end
+  
+  at_exit {
+    FastRequire.save
+  }
+  
+  def self.save
+    File.open(@@loc, 'wb'){|f| f.write Marshal.dump(@@require_locs)}  
+  end
+  
   def require lib
-    original_require lib
+    puts lib
+    if a = @@require_locs[lib]
+      puts 'miss'
+      eval File.open(a, 'rb') {|f| f.read} # binread
+    else
+      puts 'hit'
+      
+      old = $LOADED_FEATURES.dup
+      original_require lib
+      new = $LOADED_FEATURES - old
+      @@required[lib] = new.last
+    end
   end
 end
+
+module Kernel
+  alias :original_require :require
+  extend FastRequire   # overwrite require...
+end
+
--- spec/spec.fast_require.rb
+++ spec/spec.fast_require.rb
@@ -1,21 +1,40 @@
+raise if defined?(FastRequire)
+require 'sane'
+require 'spec/autorun'
+require_relative '../lib/fast_require.rb'
+
+$: << File.dirname(__FILE__)  
+
 describe "faster requires" do
   
-  $: << File.dirname(__FILE__)
+  def with_file(filename)
+    FileUtils.touch filename + '.rb'
+    yield
+    FileUtils.rm filename + '.rb'
+  end
   
   it "should require files still" do
-    require 'file1'
+    with_file('file1') { require 'file1'}    
   end
   
   it "should require .so files still" do
-    # require ruby-prof gem
-    require 'ruby_prof'
+    # ruby-prof gem
+    require 'ruby_prof' # .so
   end
   
-  it "should use line number"
+  it "should add them to $LOADED_FEATURES" do
+    with_file('file2') {require 'file2'}
+    assert ($LOADED_FEATURES.grep(/file2.rb/)).length > 0
+  end
   
   it "should work with and without rubygems, esp. in 1.8"
   
-  it "should have a faster require method"
+  it "should have a faster require method--faster, my friend, faster!"
   
-  
+  it "should save a file" do
+    loc = File.expand_path('~/.ruby_fast_require_location')
+    FileUtils.rm loc
+    FastRequire.save
+    assert File.exist?(loc)
+  end
 end
