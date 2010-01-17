a8860695ec7be3684f11c1b0d1f3b0680c3f6011
rdp <rogerpack2005@gmail.com>
2010-01-16 21:05:45 -0700

    fix another test

--- lib/fast_require.rb
+++ lib/fast_require.rb
@@ -3,9 +3,12 @@ module FastRequire
   @@loc = File.expand_path('~/.ruby_fast_require_location')
   if File.exist? (@@loc)
     @@require_locs = Marshal.restore( File.open(@@loc, 'rb') {|f| f.read})
+    
   else
     @@require_locs = {}
   end
+  @@already_loaded = {}
+  $LOADED_FEATURES.each{|loaded| @@already_loaded[loaded] = true}
 
   at_exit {
     FastRequire.save
@@ -23,26 +26,25 @@ module FastRequire
 
   def require_cached lib
     if a = @@require_locs[lib]
-      begin
-        puts 'doing eval', a
-        if a =~ /.so$/
-          original_non_cached_require a # not much we can do there...hmm...
-        else
-          eval File.open(a, 'rb') {|f| f.read} # binread
-          $LOADED_FEATURES << a
-        end        
-      rescue => e
-        debugger
-        3
-        raise e
-      end
+      return if @@already_loaded[a]
+      @@already_loaded[a] = true
+      if a =~ /.so$/
+        puts 'doing original require on full path'
+        original_non_cached_require a # not much we can do there...too bad...
+      else
+        pss 'doing eval on ' + lib + '=>' + a
+        eval(File.open(a, 'rb') {|f| f.read}, TOPLEVEL_BINDING, a)
+        $LOADED_FEATURES << a        
+      end      
     else
       old = $LOADED_FEATURES.dup
-      puts lib.to_s      
+      pps 'searching', lib.to_s      
       if(original_non_cached_require lib)
         new = $LOADED_FEATURES - old
-        @@require_locs[lib] = new.last      
-      end# how could this fail, though?
+        @@require_locs[lib] = new.last
+        pps 'found it:' + lib + '=>' + @@require_locs[lib]
+        @@already_loaded[@@require_locs[lib]] = true
+      end# how could this fail, though...
     end
     
   end
@@ -50,7 +52,7 @@ end
 
 module Kernel
   if(defined?(@already_using_fast_require))
-    raise 'cant require it twice'
+    raise 'cant require it twice...'
   else
     @already_using_fast_require = true
   end
--- spec/spec.fast_require.rb
+++ spec/spec.fast_require.rb
@@ -1,85 +1,96 @@
 require 'faster_rubygems' if RUBY_VERSION < '1.9'
 require 'sane'
 assert !defined?(FastRequire) # so that we can loadup our unit tests sanely, using the old way LOL.
-require 'sane'
-require 'ruby-debug'
 require 'spec/autorun'
 require 'benchmark'
+require 'spec/adapters/mock_frameworks/rspec'
+#require 'ruby-debug'
 
-$: << File.dirname(__FILE__)
+require_relative '../lib/fast_require'
 
 describe "faster requires" do
 
   def with_file(filename = 'test')
-      FileUtils.touch filename + '.rb'
-      yield
-      FileUtils.rm filename + '.rb'
+    FileUtils.touch filename + '.rb'
+    yield
+    FileUtils.rm filename + '.rb'
   end
-
+  
+  
   it "should be able to go one deep" do
-    with_file do
-      require 'test' # should not blow
+    Dir.chdir('files') do
+      require 'c'
+      require 'c'
     end
   end
 
+  it "should be able to go two deep, and once only" do
+    Dir.chdir('files') do
+      puts 'starting', '', '', '', ''
+      require 'a_requires_b'
+      require 'a_requires_b'
+      require 'a_requires_b'
+      $b.should == 1 # not require it twice...
+    end
+  end
 
   it "should be faster" do
-    slow = Benchmark.realtime { system("#{OS.ruby_bin} slow.rb")}
-    Benchmark.realtime { system("#{OS.ruby_bin} fast.rb")} # warmup
-    fast = Benchmark.realtime { system("#{OS.ruby_bin} fast.rb")}
-    pps 'fast', fast, 'slow', slow
-    assert fast*2 < slow
+    Dir.chdir('files') do
+      slow = Benchmark.realtime { system("#{OS.ruby_bin} slow.rb")}
+      Benchmark.realtime { system("#{OS.ruby_bin} fast.rb")} # warmup
+      fast = Benchmark.realtime { system("#{OS.ruby_bin} fast.rb")}
+      pps 'fast', fast, 'slow', slow
+      assert fast*2 < slow
+    end
   end
-  
+
   it "should not save if it hasn't changed"
   it "should cache when requires have already been done instead of calling require on them again"
   it "should know which requires are currently active and avoid calling require on them again"
 
+  it "should have different based on $0"
+
+
   it "should do two of the same requires" do
     with_file('test') do
       require 'test'
-      require 'test'      
+      require 'test'
     end
   end
 
-    require_relative '../lib/fast_require'
-    before do # each
-      ::FastRequire.clear!
-    end
+  before do # each
+    ::FastRequire.clear!
+  end
 
-    it "should be able to go two deep" do
-      
-    end
 
-    
-    it "should require files still" do
-      with_file('file1') { require 'file1'}
-    end
+  it "should require files still" do
+    with_file('file1') { require 'file1'}
+  end
 
-    it "should require .so files still" do
-      # ruby-prof gem
-      2.times { require 'ruby_prof' } # .so
-    end
+  it "should require .so files still" do
+    # ruby-prof gem
+    2.times { require 'ruby_prof' } # .so
+  end
 
-    it "should add them to $LOADED_FEATURES" do
-      with_file('file2') {require 'file2'}
-      assert ($LOADED_FEATURES.grep(/file2.rb/)).length > 0
-    end
+  it "should add them to $LOADED_FEATURES" do
+    with_file('file2') {require 'file2'}
+    assert ($LOADED_FEATURES.grep(/file2.rb/)).length > 0
+  end
 
-    it "should work with and without rubygems, esp. in 1.8" do
-      # run these tests in 1.8...hmm...
-      # maybe with and
-    end
+  it "should work with and without rubygems, esp. in 1.8" do
+    # run these tests in 1.8...hmm...
+    # maybe with and
+  end
 
-    it "should have a faster require method--faster, my friend, faster!"
+  it "should have a faster require method--faster, my friend, faster!"
 
-    it "should save a file" do
-      FastRequire.clear!
-      loc = File.expand_path('~/.ruby_fast_require_location')
-      assert !File.exist?(loc)
-      FastRequire.save
-      assert File.exist?(loc)
-    end
+  it "should save a file" do
+    FastRequire.clear!
+    loc = File.expand_path('~/.ruby_fast_require_location')
+    assert !File.exist?(loc)
+    FastRequire.save
+    assert File.exist?(loc)
+  end
 
-    it "should override the gem method"
+  it "should override the gem method"
 end
