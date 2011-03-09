cd9a4d08cebebe1f3efa89950861e2ad4c9d2991
rogerdpack <rogerpack2005@gmail.com>
2011-03-09 11:56:22 -0700

    specz

--- spec/files/load_various_gems3.rb
+++ spec/files/load_various_gems3.rb
@@ -1,6 +1,5 @@
 $: << '.'
 $: << 'files'
-require 'gem_before.rb'
-require 'ruby-debug'
+require 'gem_after.rb'
 require 'rspec'
 # d:/Ruby192/lib/ruby/gems/1.9.1/gems/diff-lcs-1.1.2/lib/diff/lcs/callbacks.rb:53: warning: already initialized constant SequenceCallbacks
