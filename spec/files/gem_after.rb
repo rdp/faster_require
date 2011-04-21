56b6aa35958e52f3e17663bc7afb95af24b372eb
rogerdpack <rogerpack2005@gmail.com>
2011-04-21 11:49:07 -0600

    clean

--- spec/files/gem_after.rb
+++ spec/files/gem_after.rb
@@ -8,4 +8,3 @@ raise if FastRequire.already_loaded.to_a.flatten.grep(/files\/b.rb/).length > 0
 require 'files/b.rb'
 raise if(require 'files/b.rb')
 raise 'lacking b.rb ' + FastRequire.already_loaded.to_a.join(' ') unless FastRequire.already_loaded.to_a.flatten.grep(/files\/b.rb/).length > 0
-p 'success'
